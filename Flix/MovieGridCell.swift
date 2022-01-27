478304f01282ed181a7c84c730c3740de4df41c2
Dsangha01 <70395817+Dsangha01@users.noreply.github.com>
2022-01-27 03:43:01 -0500

    Main Push

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,20 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Dhanpreet Sangha on 1/27/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+    
+    
+    
+}
